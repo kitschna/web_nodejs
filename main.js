32dab7a60ecd2097ec02c2d397b4fb85d6047b7b
kitschna <gichang.ee@gmail.com>
2021-02-04 23:56:19 +0900

    complete 19.2

--- main.js
+++ main.js
@@ -6,41 +6,65 @@ var app = http.createServer(function(request,response){
 		var _url = request.url;
 		var queryData = url.parse(_url, true).query;
 		var title = queryData.id
-		console.log(_url)
-		console.log(queryData)
-		console.log(queryData.id)
-		if(_url == '/'){
-			title = 'Welcome'
-    }
-    if(_url == '/favicon.ico'){
-      return response.writeHead(404);
-    }
-		response.writeHead(200);
-		fs.readFile(`data/${queryData.id}`, 'utf8',
-		function(err, description){
-			var template = `
-			<!doctype html>
-			<html>
-			<head>
-				<title>WEB1 - ${title}</title>
-				<meta charset="utf-8">
-			</head>
-			<body>
-				<h1><a href="/">${title}</a></h1>
-				<ul>
-					<li><a href="/?id=HTML">HTML</a></li>
-					<li><a href="/?id=CSS">CSS</a></li>
-					<li><a href="/?id=HTML">JavaScript</a></li>
-				</ul>
-				<h2>${title}</h2>
-				<p>${description}</p>
-				<p style="margin-top:45px;">HTML elements are the building blocks of HTML pages. With HTML constructs, images and other objects, such as interactive forms, may be embedded into the rendered page. It provides a means to create structured documents by denoting structural semantics for text such as headings, paragraphs, lists, links, quotes and other items. HTML elements are delineated by tags, written using angle brackets.
-				</p>
-			</body>
-			</html>
-			`;
-			response.end(template);
-		})
-});
+		var pathname = url.parse(_url, true).pathname;
+	
+	if(pathname === '/'){
+		if(queryData.id === undefined){
+			fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
+				var title = 'Welcome';
+				var description = 'Hello, Node.js'
+				var template = `
+				<!doctype html>
+				<html>
+				<head>
+					<title>WEB1 - ${title}</title>
+					<meta charset="utf-8">
+				</head>
+				<body>
+					<h1><a href="/">${title}</a></h1>
+					<ul>
+						<li><a href="/?id=HTML">HTML</a></li>
+						<li><a href="/?id=CSS">CSS</a></li>
+						<li><a href="/?id=HTML">JavaScript</a></li>
+					</ul>
+					<h2>${title}</h2>
+					<p>${description}</p>
+				</body>
+				</html>
+				`;
+				response.writeHead(200);
+				response.end(template);
+			});
+		} else {
+			fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
+				var title = queryData.id;
+				var template = `
+				<!doctype html>
+				<html>
+				<head>
+					<title>WEB1 - ${title}</title>
+					<meta charset="utf-8">
+				</head>
+				<body>
+					<h1><a href="/">${title}</a></h1>
+					<ul>
+						<li><a href="/?id=HTML">HTML</a></li>
+						<li><a href="/?id=CSS">CSS</a></li>
+						<li><a href="/?id=HTML">JavaScript</a></li>
+					</ul>
+					<h2>${title}</h2>
+					<p>${description}</p>
+				</body>
+				</html>
+				`;
+				response.writeHead(200);
+				response.end(template);
+			})
+		}	
+	} else {
+		response.writeHead(404);
+		response.end('not found');
+	}
+})
 
-app.listen(3000); //3000 포트에 nodejs를 실행! 
+app.listen(3000);
